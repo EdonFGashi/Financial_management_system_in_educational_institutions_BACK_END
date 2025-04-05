41bcbf46f5c96516a2048a7b85efe83900c40fd6
EdonFGashi <edongashi2004@outlook.com>
2025-04-05 16:09:36 +0200

    testing branch

---
+++ Financial_management_system_in_educational_institutions_API/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace Financial_management_system_in_educational_institutions_API.Models
+{
+    public class Account
+    {
+        //kur e krijojme nje tabele
+        public int accId { get; set; }
+        public string organisationName { get; set; }
+        public string role { get; set; }
+        public string username { get; set; }
+        public string passwordHash { get; set; }
+        public string salt { get; set; }
+        public int twoFAcode { get; set; }
+        public DateTime twoFAtime { get; set; }
+    }
+}
