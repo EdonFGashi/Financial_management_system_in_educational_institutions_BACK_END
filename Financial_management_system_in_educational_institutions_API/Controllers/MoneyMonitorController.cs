2d00ff1ff4f31a2f5b51386d682a2b9e1c39164b
EdonFGashi <edongashi2004@outlook.com>
2025-04-06 15:18:06 +0200

    finalizing end point methods for get, get by id, post, delete, put, patch. These all works with DB

--- Financial_management_system_in_educational_institutions_API/Controllers/MoneyMonitorController.cs
+++ Financial_management_system_in_educational_institutions_API/Controllers/MoneyMonitorController.cs
@@ -4,6 +4,7 @@ using Financial_management_system_in_educational_institutions_API.Models.Dto;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Financial_management_system_in_educational_institutions_API.Controllers
 {
@@ -11,13 +12,17 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
     [ApiController]
     public class MoneyMonitorController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        public MoneyMonitorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-
-        public ActionResult<IEnumerable<UserDto>> GetUsers() //Me interfacen ActionResult mund te kthejme qfaredo tipi te te dhenave
+        public ActionResult<IEnumerable<AccountDto>> GetUsers() //Me interfacen ActionResult mund te kthejme qfaredo tipi te te dhenave
         {
-            return Ok(UsersStorage.usersList); //e kthejme status kodin 200 OK bashk me objektet e kerkuara
+            return Ok(_db.tblAccounts); //e kthejme status kodin 200 OK bashk me objektet e kerkuara
         }
 
 
@@ -29,8 +34,8 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
         //[ProducesResponseType(200, Type=typeof(UserDto))] // mund te caktohet edhe tipi i objektit qe e kthen, morepo kete e kemi caktuar ne metode <UserDto>
 
 
-        //meqe e kthen vetem nje rekors e largojme IEnumerable
-        public ActionResult<UserDto> GetUser(int id)
+        //meqe e kthen vetem nje rekord e largojme IEnumerable
+        public ActionResult<AccountDto> GetUser(int id)
         {
             //validime
             if(id == 0)
@@ -39,7 +44,7 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
             }
 
             //kur kerkojme user me nje id te caktuar ateher para se ta kthejme e ruajme ne nje variabel dhe e kontrollojme nese esht valid
-            var user = UsersStorage.usersList.FirstOrDefault(u => u.UserId == id);
+            var user = _db.tblAccounts.FirstOrDefault(u => u.accId == id);
             if (user == null)
             {
                 return NotFound(); //status kodi per 404 not found
@@ -51,27 +56,40 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<UserDto> CreateUser([FromBody] UserDto userDto)//kete objekt e marrim nga body
+        public ActionResult<AccountDto> CreateUser([FromBody] AccountDto accountDto)//kete objekt e marrim nga body
         { 
             //kthimi i error-eve te personalizuara, psh nese nuk deshirojme qe useri me emrin e njejte te insertohet 2 here
-            if(UsersStorage.usersList.FirstOrDefault(u=>u.FirstName.ToLower() == userDto.FirstName.ToLower()) != null)
+            if(_db.tblAccounts.FirstOrDefault(u=>u.username.ToLower() == accountDto.username.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "User with this name already exists !");
                 return BadRequest(ModelState);
             }
-            if (userDto == null)
+            if (accountDto == null)
             {
-                return BadRequest(userDto);
+                return BadRequest(accountDto);
             }
-            if (userDto.UserId > 0) //kur krijohet nje user i ri ID-ja e tij duhet te jete 0 nga fron end-i sepse ne DB e kemi ID-ne autoincrement 
+            if (accountDto.accId > 0) //kur krijohet nje user i ri ID-ja e tij duhet te jete 0 nga fron end-i sepse ne DB e kemi ID-ne autoincrement 
             {
                return StatusCode(StatusCodes.Status500InternalServerError); //kthimi i nje status code qe nuk e ofron ActionResult
             }
-            userDto.UserId = UsersStorage.usersList.OrderByDescending(u => u.UserId).FirstOrDefault().UserId + 1;//(ID llogaritet ketu ne back end)
-            UsersStorage.usersList.Add(userDto);
+
+            //accountDto.accId = _db.tblAccounts.OrderByDescending(u => u.accId).FirstOrDefault().accId + 1;//(ID llogaritet ketu ne back end)
+            Account model = new()
+            {
+                accId = accountDto.accId,
+                organisationName = accountDto.organisationName,
+                role = accountDto.role,
+                username = accountDto.username,
+                passwordHash = accountDto.passwordHash,
+                salt = "",
+                twoFAcode = accountDto.twoFAcode,
+                twoFAtime = DateTime.Now
+            };
+            _db.tblAccounts.Add(model);
+            _db.SaveChanges(); //kjo metode e ruan ne DB dhe e dergon ne DB, mund te insertojme disa rekorde me .Add() dhe ne fund ta therrasim SaveChanges()
 
             //ne metodat qe krijojne nje resurs (post) nuk mjafton qe te kthehet vetem 200OK, mirepo mundemi qe ta kthejme edhe url se ku u krijua aj objekt
-            return CreatedAtRoute("GetUser", new { id = userDto.UserId }, userDto);
+            return CreatedAtRoute("GetUser", new { id = accountDto.accId }, accountDto);
         }
 
 
@@ -85,12 +103,13 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
             {
                 return BadRequest();
             }
-            var user = UsersStorage.usersList.FirstOrDefault(u=>u.UserId == id);
+            var user = _db.tblAccounts.FirstOrDefault(u=>u.accId == id);
             if (user == null)
             {
                 return NotFound();
             }
-            UsersStorage.usersList.Remove(user);
+            _db.tblAccounts.Remove(user);
+            _db.SaveChanges();
             return NoContent();
         }
 
@@ -100,20 +119,36 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
         [HttpPut("{id:int}", Name = "UpdateUser")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult UpdateUser(int id, [FromBody]UserDto userDto)
+        public IActionResult UpdateUser(int id, [FromBody]AccountDto accountDto)
         {
-            if(userDto == null || id != userDto.UserId)
+            if(accountDto == null || id != accountDto.accId)
             {
                 return BadRequest();
             }
-            var user = UsersStorage.usersList.FirstOrDefault(u => u.UserId == id);
-            user.FirstName = userDto.FirstName;
-            user.LastName = userDto.LastName;
-            user.BrirthDate = userDto.BrirthDate;
-            user.Gender = userDto.Gender;
-            user.City = userDto.City;
-            user.Nationality = userDto.Nationality;
-            
+            //var user = UsersStorage.usersList.FirstOrDefault(u => u.UserId == id);
+            //user.FirstName = userDto.FirstName;
+            //user.LastName = userDto.LastName;
+            //user.BrirthDate = userDto.BrirthDate;
+            //user.Gender = userDto.Gender;
+            //user.City = userDto.City;
+            //user.Nationality = userDto.Nationality;
+
+            //Pa EF eshte dashur qe te nxjerrim nga DB dhe ti bejme update nje nga nje
+
+            //Me EF nuk kemi nevoje, thjesht e konvertojme AccountDto ne Account dhe e ruajme ne DB
+            Account model = new()
+            {
+                accId = accountDto.accId,
+                organisationName = accountDto.organisationName,
+                role = accountDto.role,
+                username = accountDto.username,
+                passwordHash = accountDto.passwordHash,
+                salt = "",
+                twoFAcode = accountDto.twoFAcode,
+                twoFAtime = DateTime.Now
+            };
+            _db.tblAccounts.Update(model); //update e ben EF automatikisht
+            _db.SaveChanges();
             return NoContent();
         }
 
@@ -121,18 +156,51 @@ namespace Financial_management_system_in_educational_institutions_API.Controller
         [HttpPatch("{id:int}", Name = "UpdatePartialUser")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult UpdatePartialUser(int id, JsonPatchDocument<UserDto> patchDto)
+        public IActionResult UpdatePartialUser(int id, JsonPatchDocument<AccountDto> patchDto)
         {
             if(patchDto == null || id == 0)
             {
                 return BadRequest();
             }
-            var user = UsersStorage.usersList.FirstOrDefault(u=>u.UserId==id);
-            if(user == null)
+            //e marrim userin nga DB dhe e ruajme ne nje variabel dhe e konvertojme ne AccountDto
+            //ne patch nuk ndryshohet i tere objekti por vetem ndonje vlere e tij.
+            var user = _db.tblAccounts.AsNoTracking().FirstOrDefault(u=>u.accId==id);
+            AccountDto userDto = new()
+            {
+                accId = user.accId,
+                organisationName = user.organisationName,
+                role = user.role,
+                username = user.username,   
+                passwordHash = user.passwordHash,
+                //salt
+                twoFAcode = user.twoFAcode,
+                twoFAtime = DateTime.Now
+            };
+            if (user == null)
             {
                 return NotFound();
             }
-            patchDto.ApplyTo(user, ModelState); //apliko ndrshimet ne objektin user, nese ka ndonje error atehere ruaje ne ModelState
+            //qdo ndryshim qe ka ardhur nga front ne pachDto, eshte i tipit AccountDto do ti aplikojme ne kete variablen lokale userDto
+
+            //patchDto.ApplyTo(userDto, ModelState); //apliko ndrshimet ne objektin user, nese ka ndonje error atehere ruaje ne ModelState
+            patchDto.ApplyTo(userDto, ModelState);
+
+            //mirepo kur ta bejme udate ne DB, duhet te jete i tipit tblAccounts prandaj e konvertojme prap ne tblAccounts
+            Account model = new()
+            {
+                accId = userDto.accId,
+                organisationName = userDto.organisationName,
+                role = userDto.role,
+                username = userDto.username,
+                passwordHash = userDto.passwordHash,
+                salt = "",
+                twoFAcode = userDto.twoFAcode,
+                twoFAtime = DateTime.Now
+            };
+
+            _db.tblAccounts.Update(model); //update e ben EF automatikisht
+            _db.SaveChanges();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest();
