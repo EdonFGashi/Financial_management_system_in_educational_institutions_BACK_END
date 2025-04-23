cd99354004f136bd73ac7273cad81354ce2ebcb4
Art Kelmendi <art.kelmendi@student.uni-pr.edu>
2025-04-23 13:01:12 +0200

    Added KomunaDto.cs

--- Financial_management_system_in_educational_institutions_API/Models/Dto/KomunaDto.cs
+++ Financial_management_system_in_educational_institutions_API/Models/Dto/KomunaDto.cs
@@ -1,1 +1,12 @@
-KomunaDto.cs
+namespace Financial_management_system_in_educational_institutions_API.Models.Dto
+{
+    public class KomunaDto
+    {
+        public int komunaId { get; set; }
+        public string qyteti { get; set; }
+        public int nrPopullsis { get; set; }
+        public decimal buxhetiAktual { get; set; }
+        public bool ditaNdarjesAuto { get; set; }
+        public int accId { get; set; }
+    }
+}
