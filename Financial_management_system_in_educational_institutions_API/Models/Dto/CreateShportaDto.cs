4f3005cd2a148543837ea2cfdfd62d81f1638277
Olta Olloni <oltaxx@gmail.com>
2025-05-04 15:11:41 +0200

    Add Shporta model and Dto

---
+++ Financial_management_system_in_educational_institutions_API/Models/Dto/CreateShportaDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Financial_management_system_in_educational_institutions_API.Models.Dto
+{
+    public class CreateShportaDto
+    {
+        public int ProduktiId { get; set; }
+        public int Sasia { get; set; }
+    }
+}
---
+++ Financial_management_system_in_educational_institutions_API/Models/Shporta.cs
@@ -0,0 +1,19 @@
+﻿namespace Financial_management_system_in_educational_institutions_API.Models
+{
+    public class Shporta
+    {
+        public int Id { get; set; }
+
+        public int ProduktiId { get; set; }
+        public Produkti Produkti { get; set; }
+
+        public int ShkollaId { get; set; }
+        public Shkolla Shkolla { get; set; }
+
+        public int Sasia { get; set; }
+
+        public string Statusi { get; set; } // "Aprovuar", "Jo Aprovuar"
+
+        public DateTime DataShtimit { get; set; } = DateTime.UtcNow;
+    }
+}
