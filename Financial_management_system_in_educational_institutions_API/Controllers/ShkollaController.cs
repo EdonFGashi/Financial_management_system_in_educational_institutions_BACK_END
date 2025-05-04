60c74d33fcc655d6913ecd73bd80e30c86c1f9f4
OraOsmani <ora.osmani@wiso-tech.de>
2025-05-05 00:18:08 +0200

    fix shkolla service for tentants

--- Financial_management_system_in_educational_institutions_API/Controllers/ShkollaController.cs
+++ Financial_management_system_in_educational_institutions_API/Controllers/ShkollaController.cs
@@ -1,65 +1,50 @@
 using Financial_management_system_in_educational_institutions_API.Interfaces;
-using Financial_management_system_in_educational_institutions_API.Models;
 using Financial_management_system_in_educational_institutions_API.Models.Dto.Shkolla;
-using Financial_management_system_in_educational_institutions_API.Models.Shared;
-using Financial_management_system_in_educational_institutions_API.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Financial_management_system_in_educational_institutions_API.Controllers
+[ApiController]
+[Route("api/{schemaName}/[controller]")]
+public class ShkollaController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ShkollaController : ControllerBase
+    private readonly IShkollaService _service;
+
+    public ShkollaController(IShkollaService service)
     {
-        private readonly IShkollaService _shkollaService;
+        _service = service;
+    }
 
-        public ShkollaController(IShkollaService shkollaService)
-        {
-            _shkollaService = shkollaService;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromRoute] string schemaName)
+    {
+        var result = await _service.GetAllAsync(schemaName);
+        return StatusCode(result.StatusCode, result);
+    }
 
-        [HttpGet]
-        [ProducesResponseType(typeof(Response<List<Shkolla>>), 200)]
-        public async Task<IActionResult> GetAll()
-        {
-            var result = await _shkollaService.GetAllAsync();
-            return StatusCode(result.StatusCode, result);
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById([FromRoute] string schemaName, int id)
+    {
+        var result = await _service.GetByIdAsync(schemaName, id);
+        return StatusCode(result.StatusCode, result);
+    }
 
-        [HttpGet("{id}")]
-        [ProducesResponseType(typeof(Response<Shkolla>), 200)]
-        [ProducesResponseType(typeof(Response<Shkolla>), 404)]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var result = await _shkollaService.GetByIdAsync(id);
-            return StatusCode(result.StatusCode, result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> Create([FromRoute] string schemaName, [FromBody] ShkollaDto dto)
+    {
+        var result = await _service.CreateAsync(schemaName, dto);
+        return StatusCode(result.StatusCode, result);
+    }
 
-        [HttpPost]
-        [ProducesResponseType(typeof(Response<Shkolla>), 200)]
-        [ProducesResponseType(typeof(Response<Shkolla>), 400)]
-        public async Task<IActionResult> Create([FromBody] ShkollaDto shkollaDto)
-        {
-            var result = await _shkollaService.CreateAsync(shkollaDto);
-            return StatusCode(result.StatusCode, result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update([FromRoute] string schemaName, int id, [FromBody] UpdateShkollaDto dto)
+    {
+        var result = await _service.UpdateAsync(schemaName, id, dto);
+        return StatusCode(result.StatusCode, result);
+    }
 
-        [HttpPut("{id}")]
-        [ProducesResponseType(typeof(Response<Shkolla>), 200)]
-        [ProducesResponseType(typeof(Response<Shkolla>), 404)]
-        public async Task<IActionResult> Update(int id, [FromBody] UpdateShkollaDto updatedShkolla)
-        {
-            var result = await _shkollaService.UpdateAsync(id, updatedShkolla);
-            return StatusCode(result.StatusCode, result);
-        }
-
-        [HttpDelete("{id}")]
-        [ProducesResponseType(typeof(Response<string>), 200)]
-        [ProducesResponseType(typeof(Response<string>), 404)]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var result = await _shkollaService.DeleteAsync(id);
-            return StatusCode(result.StatusCode, result);
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete([FromRoute] string schemaName, int id)
+    {
+        var result = await _service.DeleteAsync(schemaName, id);
+        return StatusCode(result.StatusCode, result);
     }
 }
