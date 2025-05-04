4eb07bbd0837600ba8cfa2fb48b1e4dd93d119e7
Olta Olloni <oltaxx@gmail.com>
2025-05-04 15:19:03 +0200

    Create Shporta Interface and update Dtos

---
+++ Financial_management_system_in_educational_institutions_API/Models/Dto/Shporta/ShportaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Financial_management_system_in_educational_institutions_API.Models.Dto.Shporta
+{
+    public class ShportaDto
+    {
+        public int Id { get; set; }
+        public int ProduktiId { get; set; }
+        public string EmriProduktit { get; set; }
+        public int Sasia { get; set; }
+        public string Statusi { get; set; }
+    }
+}
