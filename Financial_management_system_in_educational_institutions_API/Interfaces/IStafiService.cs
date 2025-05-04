6399e5c0923fd0f61b0cffbd26a668bfab89537c
OraOsmani <ora.osmani@wiso-tech.de>
2025-05-04 22:54:29 +0200

    added interface

--- Financial_management_system_in_educational_institutions_API/Interfaces/IStafiService.cs
+++ Financial_management_system_in_educational_institutions_API/Interfaces/IStafiService.cs
@@ -1,6 +1,18 @@
-﻿namespace Financial_management_system_in_educational_institutions_API.Interfaces
+﻿using Financial_management_system_in_educational_institutions_API.DTOs;
+using Financial_management_system_in_educational_institutions_API.Models.Shared;
+using Financial_management_system_in_educational_institutions_API.Models;
+using Financial_management_system_in_educational_institutions_API.Models.Dto;
+
+namespace Financial_management_system_in_educational_institutions_API.Interfaces
 {
     public interface IStafiService
     {
+        Task<PaginatedResponse<StafiShkolles>> GetAllPaginatedAsync(PaginationDTO paginationDto);
+        Task<Response<List<StafiShkolles>>> GetAllAsync();
+        Task<Response<StafiShkolles>> GetByIdAsync(int id);
+        Task<Response<StafiShkolles>> CreateAsync(StafiShkollesDto dto);
+        Task<Response<StafiShkolles>> UpdateAsync(int id, UpdateStafiShkollesDto dto);
+        Task<Response<string>> DeleteAsync(int id);
+
     }
 }
